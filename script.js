4978c86cb2996c31a1f89b4922a83b0020c93015
Kiran TJ <166934975+thewelshfiend@users.noreply.github.com>
2024-05-30 18:32:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function insert_Row() {
-    //Write your code here
-  
-  
+const tableParent = document.getElementById('sampleTable');
+const insertBtn = document.querySelector('input');
+
+function insert_Row(e)
+{
+    // e.preventDefault();
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <td>New Cell1</td>
+    <td>New Cell2</td>
+    `;
+    tableParent.prepend(row);
 }
