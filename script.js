a4604710ec04759246976cab5a7d779c1cacaf48
Kiran TJ <166934975+thewelshfiend@users.noreply.github.com>
2024-05-30 18:41:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function insert_Row()
     const row = document.createElement('tr');
     row.innerHTML = `
     <td>New Cell1</td>
-    <td>Row1 cell1</td>
+    <td>New Cell2</td>
     `;
     tableParent.prepend(row);
 }
